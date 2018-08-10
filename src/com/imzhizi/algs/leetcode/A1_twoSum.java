a66009b1b64b405b08080e6d825362129ea23ef1
imzhizi <imzhizi@outlook.com>
2018-08-10 23:11:45 +0800

    best solution for No. 1

--- src/com/imzhizi/algs/leetcode/A1_twoSum.java
+++ src/com/imzhizi/algs/leetcode/A1_twoSum.java
@@ -1,10 +1,8 @@
 package com.imzhizi.algs.leetcode;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.HashMap;
 
 /**
- * 1. twoSum
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * Eg.
@@ -14,14 +12,15 @@ import java.util.Arrays;
  */
 public class A1_twoSum {
     /**
-     * 函数描述：
-     * 无
-     * 时长：40 ms
+     * 题目分析：
+     * 很容易理解的一道题，在一个数组里寻找相加和为目标数字的两个数，并返回由其脚标组成的数组
+     *
+     * 时长：24ms / 27%
      *
      * 总结：
      * 将函数写成静态也是可以正常执行的，暂时还没发现效率上的影响，之后的题目为了方便都会写成静态的。
      */
-    public static int[] twoSum(int[] nums, int target) {
+    static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target)
@@ -31,8 +30,49 @@ public class A1_twoSum {
         return null;
     }
 
+    /**
+     * 题目分析：
+     * 使用了更高级的数据结构来处理题目，哈希的优势得到展现
+     *
+     * 运行时长：4ms / 97.82%
+     *
+     * 总结：
+     * 要善用高级数据结构
+     */
+    static int[] twoSum0(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp=target-nums[i];
+            if (map.containsKey(temp)) return new int[]{map.get(temp),i};
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+
+        /**
+         * 题目分析：
+         * 原本设想了一个两端逐渐向中间逼近的算法，但后来发现数组居然不是有序的，于是失败
+         *
+         * 运行时长：失败
+         *
+         * 总结：
+         *
+         */
+    static int[] twoSum2(int[] nums, int target) {
+        int head=0;
+        int end=nums.length-1;
+
+        while (head<end){
+            if (nums[head]+nums[end]==target) break;
+            else if (nums[head]+nums[end]>target) end--;
+            else head++;
+        }
+        return new int[]{head,end};
+    }
+
+
     public static void main(String[] args) {
-        int[] nums=twoSum(new int[]{3,2,4},6);
+        int[] nums=twoSum0(new int[]{3,3},6);
         System.out.println(nums[0] + " " + nums[1]);
     }
 }
