e189cc27351e742133ecd6b847a3b3c2cdcfdf03
imzhizi <imzhizi@outlook.com>
2018-08-11 23:15:01 +0800

    best solution for 7(Reverse Integer)和 9(Palindrome Number)

--- src/com/imzhizi/algs/leetcode/A7_ReverseInteger.java
+++ src/com/imzhizi/algs/leetcode/A7_ReverseInteger.java
@@ -1,7 +1,6 @@
 package com.imzhizi.algs.leetcode;
 
 /**
- * 7. Reverse Integer
  * Given a 32-bit signed integer, reverse digits of an integer.
  * Example
  * Input: 123
@@ -13,12 +12,13 @@ package com.imzhizi.algs.leetcode;
  */
 public class A7_ReverseInteger {
     /**
-     * 功能描述
+     * 题目分析：
      * 使用StringBuilder进行翻转，因为翻转后可能会大于整形数字，因此使用Long.valueOf()来接收翻转后的数字
-     * 时长：43ms
      *
-     * 总结
-     * int型的负数转换成正数时会有一位越界
+     * 时长：34ms / 6%
+     *
+     * 总结：
+     * 但这种方法往往效率很低，不建议使用
      */
     public static int reverse(int x) {
         long l = x;
@@ -32,29 +32,58 @@ public class A7_ReverseInteger {
     }
 
     /**
-     * 函数描述：
-     * 使用数学方法进行翻转，思路是从个位开始求余数，然后将余数变成最高位
-     * 第一个循环计算长度，第二个循环依次处理余数
-     * 时长：52ms
+     * 题目分析：
+     * 仍然从字符串入手，但是放弃抽象程度过高的 StringBuilder，转而通过 char[] 来计算
      *
-     * 总结：
-     * 没想到这种方法时间竟然比StringBuilder翻转，可见JavaAPI函数优化的非常好
+     * 运行时长：24ms / 23%
+     *
+     * 总结：无
      */
-    public static int reverse2(int x) {
-        int length = 0;
-        for (int i = x; i != 0; i /= 10) {
-            length++;
-        }
+    public static int reverse1(int x) {
         long result = 0;
-        for (; x != 0; x /= 10) {
-            int rest = x % 10;
-            result += rest * Math.pow(10, --length);
+
+        if (x > 0) {
+            char[] chars = (x + "").toCharArray();
+
+            for (int i = 0; i < chars.length; i++)
+                result += Math.pow(10, i) * (chars[i] - 48);
+        }else {
+            char[] chars = ((x*-1) + "").toCharArray();
+
+            for (int i = 0; i < chars.length; i++)
+                result += Math.pow(10, i) * (chars[i] - 48);
+
+            result*=-1;
         }
+
         if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) return 0;
         else return (int) result;
     }
 
+    /**
+     * 题目分析：
+     * 更为数学、数字的一种处理方法，通过逐次取余，累次乘上10，得到最终结果
+     * 使用 long result 的时长为 22ms，使用 int 应该可以更短，但要为溢出做另外处理
+     *
+     * 时长：21ms / 98%
+     *
+     * 总结：
+     * 当数据量足够大的时候，一点点优化也是有效果的
+     */
+    public static int reverse2(int x) {
+        int result = 0;
+
+        while (x != 0) {
+            if (Math.abs(result)>Integer.MAX_VALUE/10) return 0;
+            result = result * 10 + x % 10;
+            x /= 10;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
+        System.out.println(reverse1(2147483647));
         System.out.println(reverse2(2147483647));
     }
 }
--- src/com/imzhizi/algs/leetcode/A9_isPalindrome.java
+++ src/com/imzhizi/algs/leetcode/A9_isPalindrome.java
@@ -10,29 +10,28 @@ public class A9_isPalindrome {
     /**
      * 函数描述：
      * 使用StringBuilder的reverse函数进行翻转，然后使用equals函数进行比较
-     * 运行时长：未知
+     *
+     * 运行时长：102ms / 52%
      *
      * 总结：
-     * 使用JavaAPI函数，非常简单
+     * 使用 JavaAPI 函数，非常简单
      */
     public static boolean isPalindrome(int x) {
-        if (x >= 0) {
-            StringBuilder string = new StringBuilder(Long.toString(x));
-            return string.reverse().equals(string);
-        } else {
-            return false;
-        }
+        if (x < 0) return false;
+        String reverse=new StringBuilder(x + "").reverse().toString();
+        return reverse.equals(x + "");
     }
 
     /**
-     * 函数描述：
+     * 题目分析：
      * 手动使用char[]进行对称检查，若对称则回文
-     * 运行时长：未知
+     *
+     * 运行时长：100ms / 70%
      *
      * 总结：
-     * 无
+     * 手动做总是能获得更好的效果
      */
-    public static boolean isPalindrome2(int x) {
+    public static boolean isPalindrome1(int x) {
         if (x < 0) return false;
         char[] chars = String.valueOf(x).toCharArray();
         for (int i = 0; i < chars.length / 2; i++) {
@@ -41,7 +40,31 @@ public class A9_isPalindrome {
         return true;
     }
 
-    public static void main(String[] args) {
-        System.out.println(A9_isPalindrome.isPalindrome2(2147447412));//isPalindrome2
+    /**
+     * 题目分析：
+     * 根据上一道题，可以把数字先进行翻转，然后再将翻转结果和原数字对比，相等即为回文数
+     *
+     * 运行时长：96ms /94%
+     *
+     * 总结：
+     * 数字方法总是能获得更好的效率
+     */
+    public static boolean isPalindrome2(int x) {
+        if (x < 0) return false;
+        int reverse = 0;
+        int origin=x;
+
+        while (x != 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+
+        return reverse==origin?true:false;
+    }
+
+        public static void main(String[] args) {
+//        System.out.println(A9_isPalindrome.isPalindrome2(10));//isPalindrome2
+//        System.out.println(A9_isPalindrome.isPalindrome(121));//isPalindrome2
+        System.out.println(A9_isPalindrome.isPalindrome2(121));//isPalindrome2
     }
 }
