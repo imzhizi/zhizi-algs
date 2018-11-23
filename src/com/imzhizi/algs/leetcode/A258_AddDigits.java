29bfeb6b9920e481eeafd82f6a73399f1c3b42ca
imzhizi <imzhizi@outlook.com>
2018-11-23 15:26:26 +0800

    258&263

---
+++ src/com/imzhizi/algs/leetcode/A258_AddDigits.java
@@ -0,0 +1,29 @@
+package com.imzhizi.algs.leetcode;
+
+/**
+ * created by zhizi on 2018-11-23
+ */
+class A258_AddDigits {
+    /**
+     * 题目分析：
+     * 每次计算出一个 sum 是一个循环，直到 sum 是个位数也就是大于9就循环
+     * 内部假如 num 大于 0 就继续计算 sum，然后 num 除以 10 去掉个位数
+     *
+     * 运行时长：1ms /100%
+     *
+     * 总结：
+     * 无
+     */
+    public int addDigits(int num) {
+        int sum = num;
+        while (sum > 9) {
+            num = sum;
+            sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+        }
+        return sum;
+    }
+}
