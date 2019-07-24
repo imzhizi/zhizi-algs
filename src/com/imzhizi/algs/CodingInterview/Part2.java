6613a4e0a4f9507f7a8a1d92f171a25eb21c0743
质子 <imzhizi@outlook.com>
2019-07-24 22:34:40 +0800

    50 第一个只出现一次的字符

--- src/com/imzhizi/algs/CodingInterview/Part2.java
+++ src/com/imzhizi/algs/CodingInterview/Part2.java
@@ -3,6 +3,7 @@ package com.imzhizi.algs.CodingInterview;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Part2 {
@@ -31,5 +32,81 @@ public class Part2 {
         }
 
         return sb.toString();
+    }
+
+    @Test
+    public void No45() {
+
+    }
+
+    /**
+     * https://www.nowcoder.com/practice/1c82e8cf713b4bbeb2a5b31cf5b0417c
+     * 第一个只出现一次的字符的位置
+     */
+    @Test
+    public void No46() {
+
+    }
+
+    public int FirstNotRepeatingChar(String str) {
+        char[] cs=str.toCharArray();
+        int[] record = new int[58];
+
+        int length=cs.length;
+
+        int[] count = new int[58];
+        Arrays.fill(count,length);
+
+        for(int i=0;i<length;i++){
+            record[cs[i]-'A']++;
+            if(record[cs[i]-'A']==1){
+                count[cs[i]-'A']=i;
+            }else{
+                count[cs[i]-'A']=length;
+            }
+        }
+
+        Arrays.sort(count);
+        return count[0]==length?-1:count[0];
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void No47() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void No48() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void No49() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void No50() {
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void No51() {
+
     }
 }
